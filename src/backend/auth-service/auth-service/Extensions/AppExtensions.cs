bfadb5c607352b663273d8da3477d884f9c6d794
unknown <k25650727@gmail.com>
2025-02-10 09:53:54 +0100

    commit

---
+++ src/backend/auth-service/auth-service/Extensions/AppExtensions.cs
@@ -0,0 +1,9 @@
+namespace auth_service.Extensions;
+
+public static class AppExtensions
+{
+    public static WebApplication ConfigureApp(this WebApplication webApplication)
+    {
+        return webApplication;
+    }
+}
--- src/backend/auth-service/auth-service/Extensions/BuilderExtensions.cs
+++ src/backend/auth-service/auth-service/Extensions/BuilderExtensions.cs
@@ -1,4 +1,8 @@
+using Grafana.OpenTelemetry;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 namespace auth_service.Extensions;
 
@@ -6,9 +10,10 @@ public static class BuilderExtensions
 {
     public static IHostApplicationBuilder ConfigureApp(this IHostApplicationBuilder builder)
     {
-
-        
+        builder.AddHealthCheck();
+        builder.ConfigureOpenTelemetry();
         builder.Services.AddServiceDiscovery();
+        
 
         return builder;
     }
@@ -22,8 +27,25 @@ public static class BuilderExtensions
 
     public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder)
     {
-        builder.Logging.AddOpenTelemetry
-        
+        builder.Services.AddOpenTelemetry()
+            .WithTracing(
+            configure =>
+            {
+                configure.AddHttpClientInstrumentation().AddAspNetCoreInstrumentation().AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri("http://jaeger:4317");
+                });;
+                configure.UseGrafana().AddOtlpExporter();
+            })
+            .WithMetrics(configure =>
+            {
+                configure.UseGrafana().AddPrometheusExporter();
+                
+            });
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options.UseGrafana().AddConsoleExporter();
+        });
         return builder;
     }
 }
