205279fc82e3f8a3a5d67fb9d2bb7077bfc9e64a
unknown <k25650727@gmail.com>
2025-02-14 14:58:25 +0100

    working envoy jwt token validation

--- src/backend/auth-service/auth-service/Program.cs
+++ src/backend/auth-service/auth-service/Program.cs
@@ -1,6 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using auth_service.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +36,36 @@ app.UseStaticFiles(new StaticFileOptions
     RequestPath = "/.well-known"  // Ensures requests to /.well-known/ are served correctly
 });
 app.MapGet("/", () => "Hello World!").AllowAnonymous();
-app.MapGet("/hello",[Authorize(AuthenticationSchemes = "PlayingWithSchemes")] () => "PlayingWithSchemes");
+app.MapGet("/jwt", () =>
+{   
+    string privateKeyPath = "Keys/private_key.pem";
+    var privateKey = File.ReadAllText(privateKeyPath).ToCharArray();
+    app.Logger.LogInformation($"The privatekey text: {privateKey}");
+
+    using RSA rsa = RSA.Create();
+    rsa.ImportFromPem(privateKey);
+    var credentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256)
+    {
+        CryptoProviderFactory = new CryptoProviderFactory { CacheSignatureProviders = false }
+    };
+
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+        Subject = new ClaimsIdentity([
+            new Claim(JwtRegisteredClaimNames.Sub, "user123"),
+            new Claim(JwtRegisteredClaimNames.Name, "Alice"),
+            new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+        ]),
+        Expires = DateTime.UtcNow.AddHours(1),
+        SigningCredentials = credentials,
+        Issuer = "auth-service",
+        Audience = "auth-service",
+    };
+    
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+    return tokenHandler.WriteToken(token);
+});
+app.MapGet("/hello", () => "PlayingWithSchemes").AllowAnonymous();
 app.Run();
 
